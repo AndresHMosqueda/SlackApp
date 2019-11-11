02bd14a4e41d8d501b9fa5d116c482e43df74bd8
AndresHMosqueda <andreshm94@gmail.com>
2019-11-11 00:06:06 -0600

    Working on it..

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-
+import {Link} from 'react-router-dom';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 
 class Register extends Component {
@@ -12,7 +12,7 @@ class Register extends Component {
 
     render() {
         return (
-            <Grid textAlign="center" verticalAlign="middle">
+            <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
                     <Header as="h2" icon color="orange" textAlign="center">
                         <Icon name="puzzle piece" color="orange" />
@@ -26,9 +26,11 @@ class Register extends Component {
 
                             <Form.Input fluid name="password"  icon="lock" iconPosition="left" placeholder="Password" type="password" onChange={this.handleChange}/>
 
-                            
+                            <Form.Input fluid name="passwordConfirmation"  icon="repeat" iconPosition="left" placeholder="Password Confirmation" type="password" onChange={this.handleChange}/>
+                            <Button color="orange" fluid size="large" >Submit</Button>
                         </Segment>
                     </Form>
+                    <Message>Already a user? <Link to="/login">Login</Link></Message>
                 </Grid.Column>
             </Grid>
         );
