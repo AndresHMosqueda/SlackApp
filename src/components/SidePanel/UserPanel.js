7fbfe3ce4aee9a7c9d732816175282d38057c6a3
AndresHMosqueda <andreshm94@gmail.com>
2019-11-13 23:32:19 -0600

    Adding img to dropdown

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import firebase from '../../firebase';
 import {connect} from 'react-redux';
-import { Grid, Header, Icon, HeaderContent, Dropdown } from 'semantic-ui-react';
+import { Grid, Header, Icon, HeaderContent, Dropdown, Image } from 'semantic-ui-react';
 
 class UserPanel extends Component {
 
@@ -38,6 +38,8 @@ class UserPanel extends Component {
     }
 
     render() {
+        const {user} = this.state;
+
         return (
             <Grid style={{ background: '#4c3c4c' }}>
                 <Grid.Column>
@@ -51,8 +53,11 @@ class UserPanel extends Component {
                     <Header style={{ padding: '0.25em' }} as="h4" inverted>
                         <Dropdown 
                         trigger={
-                            <span>{this.state.user.displayName}</span>
-                        } options={this.dropdownOptions()} />
+                            <span>
+                                <Image src={user.photoURL} spaced="right" avatar/>
+                                {user.displayName}
+                            </span>} 
+                        options={this.dropdownOptions()} />
                     </Header>
                 </Grid.Column>
             </Grid>
